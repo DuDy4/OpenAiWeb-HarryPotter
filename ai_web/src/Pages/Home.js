02e523e4073990be795e5ee7c20dd7dbac8b6df2
DuDy4 <danshevel@gmail.com>
2024-03-03 15:26:39 +0200

    fixed text

--- ai_web/src/Pages/Home.js
+++ ai_web/src/Pages/Home.js
@@ -7,9 +7,8 @@ export default function Home(){
         <section className="py-3 text-center container">
             {/*<div className="row py-lg-5">*/}
                 <div className="col-lg-6 col-md-8 mx-auto">
-                    <h1>Hello, This is my Blog</h1>
-                    <p>Everybody is free to read it's post.</p>
-                    <p>If you want to write a post, please sign up / sign in</p>
+                    <h1>Hello, This is my personal ChatGPT page</h1>
+                    <p>Feel free to use it as you will</p>
                     {/*<img src={myCatImg} alt="" style={{width: "80%", height:"auto"}}/>*/}
                 </div>
                 <React/>
